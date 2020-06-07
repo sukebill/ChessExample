b6a63c7fe69422e7bc9a5a589ec02a776b30efad
Vassilis Alexandrov <v.alexantrov@gmail.com>
2020-06-07 11:42:50 +0300

    -- AppDelegate - Added navigation conytroller

--- ChessMovement/ChessMovement/AppDelegate.swift
+++ ChessMovement/ChessMovement/AppDelegate.swift
@@ -10,9 +10,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let navCtrl = UINavigationController(rootViewController: Route.start.viewController)
+        navCtrl.setNavigationBarHidden(true, animated: false)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navCtrl
+        window?.makeKeyAndVisible()
         return true
     }
 }
