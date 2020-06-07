ead721f8932feaa96dfe6894415c40450b7fd8fe
Vassilis Alexandrov <v.alexantrov@gmail.com>
2020-06-07 11:37:10 +0300

    -- initial set up

---
+++ ChessMovement/ChessMovement/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ChessMovement
+//
+//  Created by Vassilis Alexandrof on 7/6/20.
+//  Copyright © 2020 VA. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
